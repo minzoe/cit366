85a807c66f4da61497a57d2c6c8bd7229f352e93
minzoe <min16009@byui.edu>
2018-05-10 11:51:20 -0600

    Lesson Activity Completed

---
+++ cms/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header.component';
+import { ContactsComponent } from './contacts/contacts.component';
+import { ContactListComponent } from './contacts/contact-list/contact-list.component';
+import { ContactsDetailComponent } from './contacts/contact-detail/contacts-detail.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    ContactsComponent,
+    ContactListComponent,
+    ContactsDetailComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ cms/src/app/contacts/contact.model.ts
@@ -0,0 +1,13 @@
+import {Injectable} from '@angular/core';
+
+@Injectable()
+export class Contact {
+
+  constructor(public contactId: string,
+              public name: string,
+              public email: string,
+              public phone: string,
+              public imageUrl: string,
+              public group: Contact[]) {
+  }
+}
