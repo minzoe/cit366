44319dbf019fefde4979c46be6714341379a2ba2
minzoe <min16009@byui.edu>
2018-05-31 10:46:23 -0600

    Lesson Activity Completed

--- cms/src/app/shared/dropdown.directive.ts
+++ cms/src/app/shared/dropdown.directive.ts
@@ -6,7 +6,7 @@ import {Directive, HostBinding, HostListener} from "@angular/core";
 export class DropdownDirective {
   @HostBinding('class.open') isOpen = false;
 
-  @HostListener('mouseenter') mouseover(eventData: Event) {
+  @HostListener('click') click(eventData: Event) {
     this.isOpen = true;
   }
 
